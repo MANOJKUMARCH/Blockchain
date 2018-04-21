5ca65fb797125f3596f18a6ab49674e06424b444
MANOJKUMARCH <33849651+MANOJKUMARCH@users.noreply.github.com>
2018-04-21 08:21:47 +0530

    Add files via upload

--- SuppluChainPOC.sol
+++ SuppluChainPOC.sol
@@ -49,10 +49,10 @@ contract Retailers{
         return(check);
     }
     
-    function retailerRegister(string _name, retailerCredibility _rC) public notAdmin{
+    function retailerRegister(string _name) public notAdmin{
         uint check1 = checkRetailerExistance(msg.sender);
         if(check1 == 0){
-            retailerDetails memory temp = retailerDetails(_name,msg.sender,_rC);
+            retailerDetails memory temp = retailerDetails(_name,msg.sender,retailerCredibility(0));
             retailerCount++;
             retailers[retailerCount] = temp;
             emit retailerRequets(_name,msg.sender,retailerCount);
@@ -79,6 +79,8 @@ contract Retailers{
 contract Orders is Retailers{
     
     enum Item {TV,Refregirator,HomeTheater,AirConditioner,Microwave}
+    enum orderStatus {placed,canceled,executed}
+    
     uint[] pricePerUnit = [2000 wei,1000 wei,1000 wei,2000 wei,1000 wei];
     
     uint units;
@@ -93,11 +95,12 @@ contract Orders is Retailers{
         Item item;
         uint quantity;
         uint orderAmount;
+        orderStatus status;
     }
     
     mapping(uint => order) orders;
     
-    event retailerOrder(address _retailersAddress,Item orderedItem, uint totalUnits,uint amountPaid);
+    event retailerOrder(address _retailersAddress,Item orderedItem, uint totalUnits);
     
     modifier onlyAdmin(){
         require(admin == msg.sender);
@@ -111,9 +114,9 @@ contract Orders is Retailers{
     
     function placeOrder(uint _item, uint _units) public payable{
         orderNumber++;
-        order memory temp = order(msg.sender,Item(_item),_units,msg.value);
+        order memory temp = order(msg.sender,Item(_item),_units,msg.value,orderStatus(0));
         orders[orderNumber] = temp;
-        //emit retailerOrder(msg.sender,orderedItem,_units,amountPayable);
+        emit retailerOrder(msg.sender,orders[orderNumber].item,_units);
     }
     
     function executeOrder(uint _orderNumber,address _companyAddress) public onlyAdmin returns(string){
@@ -125,54 +128,69 @@ contract Orders is Retailers{
         address orderRAdd = orders[_orderNumber].rAdd;
         uint retailerCheck;
         retailerCredibility rCred;
-        retailerCheck = checkRetailerExistance(orders[_orderNumber].rAdd);
-        if(retailerCheck != 0){
-            rCred = retailers[retailerCheck].credibility;
+        if(uint(orders[_orderNumber].status) == 0){
+            retailerCheck = checkRetailerExistance(orders[_orderNumber].rAdd);
+            if(retailerCheck != 0){
+                rCred = retailers[retailerCheck].credibility;
+            }else{
+                return("Retailer is not listed");
+            }
+            if(uint(rCred) == 0){
+                return("Retailer not yet accepted, contact admin");
+            }else if(uint(rCred) == 1){
+                orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
+            }else if(uint(rCred) == 2){
+                if(totalUnits > 10000){
+                    finalPayable = (amountPayable * 90)/100;
+                    orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
+                }else{
+                    orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
+                }
+            }else if(uint(rCred) == 3){
+                if(totalUnits > 10000){
+                    finalPayable = (amountPayable * 80)/100;
+                    orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
+                }else if(totalUnits > 5000 && totalUnits <= 10000){
+                    finalPayable = (amountPayable * 90)/100;
+                    orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
+                }else{
+                    orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
+                }
+            }else if(uint(rCred) == 4){
+                if(totalUnits > 10000){
+                    finalPayable = (amountPayable * 70)/100;
+                    orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
+                }else if(totalUnits > 5000 && totalUnits <= 10000){
+                    finalPayable = (amountPayable * 85)/100;
+                    orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
+                }else{
+                    orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
+                }
+            }
         }else{
-            return("Retailer is not listed");
-        }
-        if(uint(rCred) == 0){
-            return("Retailer not yet accepted, contact admin");
-        }else if(uint(rCred) == 1){
-            orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
-        }else if(uint(rCred) == 2){
-            if(totalUnits > 10000){
-                finalPayable = (amountPayable * 90)/100;
-                orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
-            }else{
-                orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
-            }
-        }else if(uint(rCred) == 3){
-            if(totalUnits > 10000){
-                finalPayable = (amountPayable * 80)/100;
-                orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
-            }else if(totalUnits > 5000 && totalUnits <= 10000){
-                finalPayable = (amountPayable * 90)/100;
-                orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
-            }else{
-                orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
-            }
-        }else if(uint(rCred) == 4){
-            if(totalUnits > 10000){
-                finalPayable = (amountPayable * 70)/100;
-                orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
-            }else if(totalUnits > 5000 && totalUnits <= 10000){
-                finalPayable = (amountPayable * 85)/100;
-                orderPayment(amountPaid,finalPayable,_companyAddress,orderRAdd,_orderNumber);
-            }else{
-                orderPayment(amountPaid,amountPayable,_companyAddress,orderRAdd,_orderNumber);
-            }
+            return("Order already executed");
         }
     }
     
     function orderPayment(uint _paid,uint _payable, address _comAd, address _orAd, uint _orNo) internal onlyAdmin returns(string){
         if(_paid >= _payable){
             _comAd.transfer(_payable);
-            return("Order Executed");
+            orders[_orNo].status = orderStatus(2);
+            orders[_orNo].orderAmount = 0;
+            return("Order Processed successfully");
             }else{
                 _orAd.transfer(_paid);
+                orders[_orNo].status = orderStatus(1);
                 orders[_orNo].orderAmount = 0;
                 return("Insufficient ammount, hence returning amount and canceling order");
             }
+    }
+    
+    function getOrderDetails(uint _orderNumber) public view returns(address _retailersAddress, Item _itemOrdered, uint _orderedQuantity, uint orderedAmount, orderStatus _orderStatus){
+        return(orders[_orderNumber].rAdd,
+                orders[_orderNumber].item,
+                orders[_orderNumber].quantity,
+                orders[_orderNumber].orderAmount,
+                orders[_orderNumber].status);
     }
 }
